9262820f7f8c826bba27ee247bb745a70077a235
Vanks7 <vank.2795@gmail.com>
2026-05-31 21:07:11 +0300

    Add files via upload

---
+++ lr1.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdio.h>
+#define PI 3.14
+int pow(float n);
+void lab1_main(void);
---
+++ lr3.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdio.h>
+#define YES  1
+#define NO 0
+#define SOGL 2
+#define GLAS 3
+int bukva(char n);
+void lab3_main(void);
